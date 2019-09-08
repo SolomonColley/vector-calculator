13dc5316662830efc5e4ef9925c4b9a3640ad8f7
Solomon Colley <solomonpcolley@gmail.com>
2019-09-07 20:06:29 -0400

    Defined some Vector2D methods.

---
+++ Vector Calculator/Vector2D.cpp
@@ -0,0 +1,87 @@
+/// <summary>
+/// This class represents a single 2D vector. A 2D vector can be
+/// set, gotten, and printed to the console using user-defined
+/// precision.
+/// <remarks>
+/// Contains functions that overload the arithmetic operators +,
+/// -, *, and /. This simplifies 2D vector arithmetic.
+/// </remarks>
+/// </summary>
+/// See <see cref="Vector2D.h"/> for function declarations.
+/// Author: Solomon Colley
+/// Since: 09/07/2019
+
+#include "Vector2D.h"
+
+Vector2D::Vector2D()
+	: x(0.0), y(1.0)
+{
+} // end default constructor
+
+Vector2D::Vector2D(const double x, const double y)
+{
+	setVector(x, y);
+} // end parameterized constructor
+
+Vector2D::~Vector2D() { } // end destructor
+
+void Vector2D::setVector(const double x, const double y)
+{
+	this->x = x;
+	this->y = y;
+} // end setVector
+
+const double Vector2D::getX()
+{
+	return x;
+} // end getX
+
+const double Vector2D::getY()
+{
+	return y;
+} // end getY
+
+const std::string Vector2D::toString()
+{
+	auto ss = std::stringstream();
+
+	// format the 2D vector without using decimal precision
+	// the format is the following: (x, y)
+	ss << "(" << x << ", " << y << ")";
+
+	return ss.str();
+} // end toString
+
+const std::string Vector2D::toString(const int precision)
+{
+	auto ss = std::stringstream();
+
+	// format the 2D vector using user-defined decimal precision
+	// the format is the following: (x, y)
+	ss << "(" << std::setprecision(precision) << std::fixed
+		<< x << ", " << std::setprecision(precision) << std::fixed
+		<< y << ")";
+
+	return ss.str();
+} // end toString
+
+const Vector2D Vector2D::operator+(const Vector2D& v)
+{
+	return Vector2D();
+} // end overloaded operator +
+
+const Vector2D Vector2D::operator-(const Vector2D& v)
+{
+	return Vector2D();
+} // end overloaded operator -
+
+const Vector2D Vector2D::operator*(const Vector2D& v)
+{
+	return Vector2D();
+} // end overloaded operator *
+
+const Vector2D Vector2D::operator/(const Vector2D& v)
+{
+	return Vector2D();
+} // end overloaded operator /
+
---
+++ Vector Calculator/Vector2D.h
@@ -0,0 +1,106 @@
+/// <summary>
+/// This class represents a single 2D vector. A 2D vector can be
+/// set, gotten, and printed to the console using user-defined
+/// precision.
+/// <remarks>
+/// Contains functions that overload the arithmetic operators +,
+/// -, *, and /. This simplifies 2D vector arithmetic.
+/// </remarks>
+/// </summary>
+/// See <see cref="Vector2D.cpp"/> for function definitions.
+/// Author: Solomon Colley
+/// Since: 09/07/2019
+
+#pragma once
+
+#include <iomanip>
+#include <string>
+#include <sstream>
+
+class Vector2D
+{
+public:
+	/// <summary>
+	/// The default constructor. Sets the vector to default values.
+	/// </summary>
+	Vector2D();
+
+	/// <summary>
+	/// A parameterized constructor. Sets the vector to the
+	/// parameter values.
+	/// </summary>
+	/// <param name="x">The 2D vector's x-value.</param>
+	/// <param name="y">The 2D vector's y-value.</param>
+	Vector2D(const double x, const double y);
+
+	/// <summary>
+	/// The destructor. Frees allocated memory.
+	/// </summary>
+	~Vector2D();
+
+	/// <summary>
+	/// Sets this 2D vector's x- and y-values using the parameter
+	/// values.
+	/// </summary>
+	/// <param name="x">The 2D vector's x-value.</param>
+	/// <param name="y">The 2D vector's y-value.</param>
+	void setVector(const double x, const double y);
+
+	/// <summary>
+	/// Gets this 2D vector's x-value.
+	/// </summary>
+	/// <returns>This 2D vector's x-value.</returns>
+	const double getX();
+
+	/// <summary>
+	/// Gets this 2D vector's y-value.
+	/// </summary>
+	/// <returns>This 2D vector's y-value.</returns>
+	const double getY();
+
+	/// <summary>
+	/// Formats this 2D vector as a string object without using
+	/// decimal precision.
+	/// </summary>
+	/// <returns>This 2D vector as a formatted string.</returns>
+	const std::string toString();
+
+	/// <summary>
+	/// Using user-defined precision, formats this 2D vector as
+	/// a string object.
+	/// </summary>
+	/// <param name="precision">User-defined decimal precision.</param>
+	/// <returns>This 2D vector as a formatted string.</returns>
+	const std::string toString(const int precision);
+
+	/// <summary>
+	/// Operator + overloader that computes the sum of 2 2D vectors.
+	/// </summary>
+	/// <param name="v">A 2D vector.</param>
+	/// <returns>The sum of 2 2D vectors.</returns>
+	const Vector2D operator+(const Vector2D& v);
+
+	/// <summary>
+	/// Operator - overloader that computes the difference of 2 2D vectors.
+	/// </summary>
+	/// <param name="v">A 2D vector.</param>
+	/// <returns>The difference of 2 2D vectors.</returns>
+	const Vector2D operator-(const Vector2D& v);
+
+	/// <summary>
+	/// Operator * overloader that computes the product of 2 2D vectors.
+	/// </summary>
+	/// <param name="v">A 2D vector.</param>
+	/// <returns>The product of 2 2D vectors.</returns>
+	const Vector2D operator*(const Vector2D& v);
+
+	/// <summary>
+	/// Operator / overloader that computes the quotient of 2 2D vectors.
+	/// </summary>
+	/// <param name="v">A 2D vector.</param>
+	/// <returns>The product of 2 2D vectors.</returns>
+	const Vector2D operator/(const Vector2D& v);
+private:
+	double x, y; // x- and y-values of the 2D vector
+}; // end Vector2D
+
